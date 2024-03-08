ed4821b8bff3721846fcb4be933f9d84b3254856
Carsten Stahl <carstenstahl307@yahoo.com>
2024-03-08 13:24:29 +0100

    made some adjustments to avoid overflow and wierd image scaling

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -4,8 +4,8 @@
 </script>
 
 <template>
-  <div class="bg-white flex-col w-[507px] h-[240px] rounded-md hover:shadow-md border-solid border-theme-gray-300 cursor-pointer group transition-all ease-in-out sm:w-[285px] sm:h-[254px] flex-grow-0 flex-shrink-0 mb-4" @click="navigate(props.to)">
-    <img :src="props.imgURL" alt="" class="rounded-md w-full h-[61.8%] saturate-0 group-hover:saturate-100 transition-all ease-in-out">
+  <div class="bg-white flex-col w-[507px] h-[240px] rounded-md hover:shadow-md border-solid border-theme-gray-300 cursor-pointer group transition-all ease-in-out sm:w-full sm:h-[254px] flex-grow-0 flex-shrink-0 mb-4" @click="navigate(props.to)">
+    <img :src="props.imgURL" alt="" class="object-cover rounded-md w-full h-[61.8%] saturate-0 group-hover:saturate-100 transition-all ease-in-out">
     <div class="font-light text-[24px] w-[80%] px-3 py-1 sm:text-[16px]">{{ props.title }}</div>
   </div>
 </template>
